431325f8d04a1090de16cd1139d8ad4bd8fb0740
Shakthi67 <shakthi.raj@exceleed.com>
2025-08-11 18:44:58 +0530

    changed route

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ClassroomProvider } from './context/ClassroomContext';
 import { ScheduleProvider } from './context/ScheduleContext';
 import Layout from './components/Layout';
