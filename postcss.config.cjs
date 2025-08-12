5a3cc2d1fd56c85fe1c324b562bf67e23f0b0e9a
Shakthi67 <shakthi.raj@exceleed.com>
2025-08-12 15:37:58 +0530

    updated postcss

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -2,14 +2,13 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    'postcss-custom-properties': { preserve: false },
     'postcss-preset-env': {
       stage: 3,
       features: {
-        'custom-properties': { preserve: false },
         'nesting-rules': true,
       },
       browsers: ['last 2 versions', 'chrome 49', 'ie 11']
     }
   }
 }
-
